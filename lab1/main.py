39bd3b231bebbc3d0cd195590fc5a98a814a0aae
via8 <viaborne8@gmail.com>
2020-09-24 21:28:24 +0300

    add tests for interval determinant

--- lab1/main.py
+++ lab1/main.py
@@ -1,5 +1,7 @@
 import numpy
+import sys
 from kaucherpy import Kaucher as interval
+from itertools import permutations
 
 def _read_input():
 	while(True):
@@ -48,6 +50,7 @@ if __name__ == "__main__":
 	elemDefault = interval(1.0 - eps, 1.0 + eps)
 	elemSpecial = interval(1.1 - eps, 1.1 + eps)
 	resultInterval = elemDefault * elemDefault - elemSpecial * elemDefault
+	print("\n")
 	print("problem 1:")
 	print("eps = " + str(eps))
 	print("result determinant interval:" + str(resultInterval))
@@ -57,6 +60,7 @@ if __name__ == "__main__":
 	else:
 		print("0 is not included")
 		print("initial matrix IS NOT special\n\n")
+	print("\n")
 
 	# problem 2 (Beck)
 	elemDiagonal = interval(1.0, 1.0)
@@ -91,4 +95,61 @@ if __name__ == "__main__":
 	else:
 		print(str(spectralRadius) + " < 1")
 		print("initial matrix IS NOT special")
+	print("\n")
 
+	# test
+	if len(sys.argv) < 2:
+		exit()
+	
+	if "--test" not in sys.argv and "-T" not in sys.argv:
+		exit()
+
+	def count_inversions(arr):
+		count = 0
+		n = len(arr)
+		for i in range(n):
+			for j in range(i + 1, n):
+				if arr[i] > arr[j]:
+					count += 1
+		return count
+
+	def determinant_by_definition(matrix):
+		if matrix is None or \
+			not hasattr(matrix, "__len__") or \
+			not hasattr(matrix[0], "__len__") or \
+			len(matrix) != len(matrix[0]):
+			return None
+		n = len(matrix)
+		# n! is quite large number
+		if n > 7:
+			return None
+		perms = permutations(range(n), n)
+		sum = interval(0, 0)
+		for perm in perms:
+			#addend = interval(1, 1)
+			addend = matrix[0][perm[0]]
+			#for i in range(n):
+			for i in range(1, n):
+				addend *= matrix[i][perm[i]]
+				if count_inversions(perm) % 2 == 0:
+					sum += addend
+				else:
+					sum -= addend
+		return sum
+
+	A1 = [[elemDefault, elemDefault], [elemSpecial, elemDefault]]
+	print("problem 1: testing determinant value...")
+	print(determinant_by_definition(A1))
+	print("\n")
+
+	A2 = [[interval(0, 0)] * n for i in range(n)]
+	for i in range(n):
+		for j in range(n):
+			if i == j:
+				A2[i][j] = elemDiagonal
+			else:
+				A2[i][j] = elemNonDiagonal
+
+	print("problem 2: testing determinant value...")
+	print(determinant_by_definition(A2))
+	print("\n")
